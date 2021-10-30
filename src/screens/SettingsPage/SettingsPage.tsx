883ca72b73352b3de74b3545f02d00968c832ffb
Chinedu25 <53890311+Chinedu25@users.noreply.github.com>
2021-10-31 00:28:30 +0100

    Seperated files to components

--- src/screens/SettingsPage/SettingsPage.tsx
+++ src/screens/SettingsPage/SettingsPage.tsx
@@ -1,132 +1,34 @@
 import "../HomePage/HomePage.css"
 import "./SettingsPage.css"
-import Logo from '../../assets/images/Logo.svg'
-import ProfileImage from '../../assets/images/ProfileImage.png'
-import SideButton from "../../components/Button/SideButton"
-import HomeIcon from "../../assets/images/HomeIcon.png"
-import SignOutIcon from "../../assets/images/SignOutIcon.png"
-import WithDrawIcon from "../../assets/images/WithdrawIcon.png"
-import SettingsIcon from "../../assets/images/SettingsIcon.png"
 import DiscordIcon from "../../assets/images/DiscordIcon.png"
-import InstagramIcon from "../../assets/images/InstagramIcon.png"
-import TikTokIcon from "../../assets/images/TikTokIcon.png"
-import YouTubeIcon from "../../assets/images/YouTubeIcon.png"
 import HelpIcon from "../../assets/images/HelpIcon.png"
-import DailyRobloxIcon from "../../assets/images/DailyRobloxIcon.png"
-import SystemRobloxIcon from "../../assets/images/SystemRobloxIcon.png"
-import BalanceIcon from "../../assets/images/BalanceIcon.png"
-import PendingIcon from "../../assets/images/PendingIcon.png"
 import SlenderGGLogoSmall from "../../assets/images/SlenderGGLogoSmall.svg"
 
-import CloseIcon from "../../assets/images/CloseIcon.svg"
-import MinimizeIcon from "../../assets/images/MinimizeIcon.svg"
+import HomeActionBar from "../../components/Header/HomeActionBar"
+import SideBar from "../../components/SideBar/SideBar"
 import {
     RouteComponentProps
 } from "react-router-dom";
 import IntensitySettings from "./IntensitySettings"
 import RenamePC from "./RenamePC"
 import FlatButton from "../../components/Button/FlatButton"
+import StatusBar from "../../components/StatusBar/StatusBar"
+
 
 type TParams = { id: string };
 
 const SettingsPage = ({ history }: RouteComponentProps<TParams>) => {
 
     //@ts-ignore
-    const ipc = window.api;
+   // const ipc = window.api;
 
- 
+
 
     return (
         <div className="backgroundContainer">
-            <div style={{ flexDirection: "row", display: "flex" }}>
-                <img className="logo" src={Logo} alt="" />
-                <div className="minimizeWinBox"
-                    onClick={() => {
-                        ipc.send("minimize")
-                    }}
-                >
-                    <img
-
-                        style={{
-                            width: 12,
-                            height: 12,
-                            cursor: "pointer"
-                        }}
-
-                        src={MinimizeIcon} alt="" />
-                </div>
-                <div className="closeWinBox"
-                    onClick={() => {
-                        ipc.send('closeApp')
-                    }}
-                >
-                    <img
-
-                        style={{
-                            width: 12,
-                            height: 12,
-                            cursor: "pointer"
-                        }}
-
-                        src={CloseIcon} alt="" />
-                </div>
-            </div>
+            <HomeActionBar />
             <div className="View">
-                <div className="sideBar">
-                    <div className="profileContainer">
-                        <img className="profileImage" src={ProfileImage} alt="" />
-                        <div style={{ marginLeft: 15, flexDirection: "column", justifyContent: "center", alignItems: "start", display: "flex" }}>
-                            <span className="username">Username 01</span>
-                            <span className="userLevel">Level: 01</span>
-                        </div>
-                    </div>
-                    <div className="sideBarButtonList">
-                        <SideButton imgSrc={HomeIcon} text={"Home"} isActive={false} onClick={() => {
-                            history.push("/homepage");
-                        }} />
-                        <SideButton imgSrc={WithDrawIcon} text={"Withdraw"} isActive={false} onClick={() => {
-                            history.push("/withdrawpage");
-                        }} />
-
-                        <SideButton imgSrc={SettingsIcon} text={"Settings"} isActive={true} onClick={() => {
-
-                        }} />
-
-                        <SideButton imgSrc={SignOutIcon} text={"Log Out"} isActive={false} onClick={() => {
-                            history.push("/logoutpage");
-                        }} />
-                    </div>
-                    <div className="getFreeRob">
-                        <span style={{ fontSize: 18, fontWeight: 600 }}>Get Free Robux!</span>
-                        <span style={{ fontSize: 14, fontWeight: 300, marginTop: 4 }}>Follow us on Social Media</span>
-                    </div>
-                    <div className="socialLinks">
-                        <div className="socialLinksRow">
-                            <div className="socialLinkButton">
-                                <span style={{ marginTop: 6 }}>Join our Discord</span>
-                                <img className="socialLinkImg" src={DiscordIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>0.25 R$</span>
-                            </div>
-                            <div className="socialLinkButton" style={{ marginLeft: 20 }}>
-                                <span style={{ marginTop: 6 }}>Subscribe on Youtube</span>
-                                <img className="socialLinkImg" src={YouTubeIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>0.25 R$</span>
-                            </div>
-                        </div>
-                        <div className="socialLinksRow">
-                            <div className="socialLinkButton">
-                                <span style={{ marginTop: 6 }}>Follow on Instagram</span>
-                                <img style={{ height: 40, width: 40, marginTop: 11 }} className="socialLinkImg" src={InstagramIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>Claimed</span>
-                            </div>
-                            <div className="socialLinkButton" style={{ marginLeft: 20 }}>
-                                <span style={{ marginTop: 6 }}>Follow on TikTok</span>
-                                <img style={{ height: 40, width: 35, marginTop: 11 }} className="socialLinkImg" src={TikTokIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>0.25 R$</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <SideBar screen={"settingspage"} history={history} />
 
                 <div className="mainInterface">
                     <h2 className="pageTitle">Settings</h2>
@@ -173,54 +75,7 @@ const SettingsPage = ({ history }: RouteComponentProps<TParams>) => {
 
                     </div>
 
-                    <div style={{ marginLeft: 0, marginTop: 21 }}>
-                        <div className="statisticsBackground">
-                            <img style={{
-                                width: 22.44,
-                                height: 25,
-                                marginLeft: 14,
-                                marginRight: 23.56
-                            }} src={DailyRobloxIcon} alt="" />
-                            <span style={{ width: 600 }} className="statisticsText">Estimated Daily Robux from Current PC</span>
-                            <span style={{ width: 100, marginLeft: 40, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">1024.32 R$</span>
-                        </div>
-                        <div className="statisticsBackground">
-                            <img style={{
-                                width: 25,
-                                height: 22,
-                                marginLeft: 14,
-                                marginRight: 23.56
-                            }} src={SystemRobloxIcon} alt="" />
-                            <span style={{ width: 600 }} className="statisticsText">Estimated Daily Robux from All Connected PCs</span>
-                            <span style={{ width: 100, marginLeft: 40, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">1536.96 R$</span>
-                        </div>
-                        <div style={{ flexDirection: "row", display: "flex" }}>
-                            <div style={{ width: 406 }} className="statisticsBackground">
-                                <img style={{
-                                    width: 14,
-                                    height: 25,
-                                    marginLeft: 14,
-                                    marginRight: 23.56
-                                }} src={PendingIcon} alt="" />
-                                <span style={{ width: 220 }} className="statisticsText">Pending Amount</span>
-                                <span style={{ width: 100, marginLeft: 10, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">128.14 R$</span>
-                            </div>
-                            <div style={{ marginLeft: 8, width: 406 }} className="statisticsBackground">
-                                <img style={{
-                                    width: 25,
-                                    height: 20,
-                                    marginLeft: 14,
-                                    marginRight: 23.56
-                                }} src={BalanceIcon} alt="" />
-                                <span style={{ width: 220 }} className="statisticsText">Balance</span>
-                                <span style={{ width: 100, marginLeft: 10, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">64.28 R$</span>
-                            </div>
-
-                        </div>
-
-
-                    </div>
-
+                    <StatusBar marginTop={21} />
                 </div>
             </div>
 
--- src/screens/WithdrawPage/WithdrawPage.tsx
+++ src/screens/WithdrawPage/WithdrawPage.tsx
@@ -1,29 +1,11 @@
 import "./WithdrawPage.css"
 import "../HomePage/HomePage.css"
-import Logo from '../../assets/images/Logo.svg'
-import ProfileImage from '../../assets/images/ProfileImage.png'
-import SideButton from "../../components/Button/SideButton"
-import HomeIcon from "../../assets/images/HomeIcon.png"
-import SignOutIcon from "../../assets/images/SignOutIcon.png"
-import WithDrawIcon from "../../assets/images/WithdrawIcon.png"
-import SettingsIcon from "../../assets/images/SettingsIcon.png"
-import DiscordIcon from "../../assets/images/DiscordIcon.png"
-import InstagramIcon from "../../assets/images/InstagramIcon.png"
-import TikTokIcon from "../../assets/images/TikTokIcon.png"
-import YouTubeIcon from "../../assets/images/YouTubeIcon.png"
+
 import HelpIcon from "../../assets/images/HelpIcon.png"
-import DailyRobloxIcon from "../../assets/images/DailyRobloxIcon.png"
-import SystemRobloxIcon from "../../assets/images/SystemRobloxIcon.png"
-import BalanceIcon from "../../assets/images/BalanceIcon.png"
-import PendingIcon from "../../assets/images/PendingIcon.png"
 import TextInput from "../../components/TextInput/TextInput"
 import ProcessingIcon from "../../assets/images/ProcessingIcon.png"
 import WithdrawalCompleteIcon from "../../assets/images/WithdrawalCompleteIcon.png"
-
-
-import CloseIcon from "../../assets/images/CloseIcon.svg"
-import MinimizeIcon from "../../assets/images/MinimizeIcon.svg"
-
+import HomeActionBar from "../../components/Header/HomeActionBar"
 
 import { useState } from "react"
 import Button from "../../components/Button/Button"
@@ -32,12 +14,14 @@ import WithdrawStatus from "./WithdrawStatus"
 //this is used to populate the history
 import fakeWithdrawHistory from "./fakeWithdrawHistory"
 
+import SideBar from "../../components/SideBar/SideBar"
 import WithdrawHistory from "./WithdrawHistory"
 
 import {
 
     RouteComponentProps
 } from "react-router-dom";
+import StatusBar from "../../components/StatusBar/StatusBar"
 
 type TParams = { id: string };
 
@@ -46,101 +30,13 @@ const WithdrawPage = ({ history }: RouteComponentProps<TParams>) => {
     const [gamePassID, setGamePassID] = useState("")
 
     //@ts-ignore
-    const ipc = window.api;
+   // const ipc = window.api;
 
     return (
         <div className="backgroundContainer">
-            <div style={{ flexDirection: "row", display: "flex" }}>
-                <img className="logo" src={Logo} alt="" />
-                <div className="minimizeWinBox" 
-                 onClick={() => {
-                        ipc.send("minimize")
-                    }}
->
-                    <img 
-                    
-
-                    style={{
-                        width: 12,
-                        height: 12,
-                        cursor: "pointer"
-                    }}
-
-                        src={MinimizeIcon} alt="" />
-                </div>
-                <div className="closeWinBox"  
-                onClick={() => {
-                        ipc.send('closeApp')
-                    }} >
-                    <img 
-                
-                    
-                    style={{
-                        width: 12,
-                        height: 12,
-                        cursor: "pointer"
-                    }}
-
-                        src={CloseIcon} alt="" />
-                </div>
-            </div>
-
+            <HomeActionBar />
             <div className="View">
-                <div className="sideBar">
-                    <div className="profileContainer">
-                        <img className="profileImage" src={ProfileImage} alt="" />
-                        <div style={{ marginLeft: 15, flexDirection: "column", justifyContent: "center", alignItems: "start", display: "flex" }}>
-                            <span className="username">Username 01</span>
-                            <span className="userLevel">Level: 01</span>
-                        </div>
-                    </div>
-                    <div className="sideBarButtonList">
-                        <SideButton imgSrc={HomeIcon} text={"Home"} isActive={false} onClick={() => {
-                            history.push("/homepage");
-                        }} />
-                        <SideButton imgSrc={WithDrawIcon} text={"Withdraw"} isActive={true} onClick={() => {
-
-                        }} />
-
-                        <SideButton imgSrc={SettingsIcon} text={"Settings"} isActive={false} onClick={() => {
-                            history.push("/settingspage");
-                        }} />
-
-                        <SideButton imgSrc={SignOutIcon} text={"Log Out"} isActive={false} onClick={() => {
-                            history.push("/logoutpage");
-                        }} />
-                    </div>
-                    <div className="getFreeRob">
-                        <span style={{ fontSize: 18, fontWeight: 600 }}>Get Free Robux!</span>
-                        <span style={{ fontSize: 14, fontWeight: 300, marginTop: 4 }}>Follow us on Social Media</span>
-                    </div>
-                    <div className="socialLinks">
-                        <div className="socialLinksRow">
-                            <div className="socialLinkButton">
-                                <span style={{ marginTop: 6 }}>Join our Discord</span>
-                                <img className="socialLinkImg" src={DiscordIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>0.25 R$</span>
-                            </div>
-                            <div className="socialLinkButton" style={{ marginLeft: 20 }}>
-                                <span style={{ marginTop: 6 }}>Subscribe on Youtube</span>
-                                <img className="socialLinkImg" src={YouTubeIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>0.25 R$</span>
-                            </div>
-                        </div>
-                        <div className="socialLinksRow">
-                            <div className="socialLinkButton">
-                                <span style={{ marginTop: 6 }}>Follow on Instagram</span>
-                                <img style={{ height: 40, width: 40, marginTop: 11 }} className="socialLinkImg" src={InstagramIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>Claimed</span>
-                            </div>
-                            <div className="socialLinkButton" style={{ marginLeft: 20 }}>
-                                <span style={{ marginTop: 6 }}>Follow on TikTok</span>
-                                <img style={{ height: 40, width: 35, marginTop: 11 }} className="socialLinkImg" src={TikTokIcon} alt="" />
-                                <span style={{ marginTop: 11 }}>0.25 R$</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <SideBar screen={"withdrawpage"} history={history} />
 
                 <div className="mainInterface">
                     <h2 className="pageTitle">Withdraw</h2>
@@ -188,11 +84,11 @@ const WithdrawPage = ({ history }: RouteComponentProps<TParams>) => {
                                 {
                                     fakeWithdrawHistory.length > 0 ?
 
-                                        fakeWithdrawHistory.slice(fakeWithdrawHistory.length-4,fakeWithdrawHistory.length).map((data, key) => {
+                                        fakeWithdrawHistory.slice(fakeWithdrawHistory.length - 4, fakeWithdrawHistory.length).map((data, key) => {
 
                                             return (
                                                 //@ts-ignore
-                                                <WithdrawHistory key={key} message={data.message}title={data.title} amount={data.amount} date={data.date} type={data.type}
+                                                <WithdrawHistory key={key} message={data.message} title={data.title} amount={data.amount} date={data.date} type={data.type}
                                                 />
                                             )
                                         }).reverse()
@@ -228,54 +124,7 @@ const WithdrawPage = ({ history }: RouteComponentProps<TParams>) => {
 
                         </div>
                     </div>
-                    <div style={{ marginLeft: 0, marginTop: 13 }}>
-                        <div className="statisticsBackground">
-                            <img style={{
-                                width: 22.44,
-                                height: 25,
-                                marginLeft: 14,
-                                marginRight: 23.56
-                            }} src={DailyRobloxIcon} alt="" />
-                            <span style={{ width: 600 }} className="statisticsText">Estimated Daily Robux from Current PC</span>
-                            <span style={{ width: 100, marginLeft: 40, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">1024.32 R$</span>
-                        </div>
-                        <div className="statisticsBackground">
-                            <img style={{
-                                width: 25,
-                                height: 22,
-                                marginLeft: 14,
-                                marginRight: 23.56
-                            }} src={SystemRobloxIcon} alt="" />
-                            <span style={{ width: 600 }} className="statisticsText">Estimated Daily Robux from All Connected PCs</span>
-                            <span style={{ width: 100, marginLeft: 40, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">1536.96 R$</span>
-                        </div>
-                        <div style={{ flexDirection: "row", display: "flex" }}>
-                            <div style={{ width: 406 }} className="statisticsBackground">
-                                <img style={{
-                                    width: 14,
-                                    height: 25,
-                                    marginLeft: 14,
-                                    marginRight: 23.56
-                                }} src={PendingIcon} alt="" />
-                                <span style={{ width: 220 }} className="statisticsText">Pending Amount</span>
-                                <span style={{ width: 100, marginLeft: 10, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">128.14 R$</span>
-                            </div>
-                            <div style={{ marginLeft: 8, width: 406 }} className="statisticsBackground">
-                                <img style={{
-                                    width: 25,
-                                    height: 20,
-                                    marginLeft: 14,
-                                    marginRight: 23.56
-                                }} src={BalanceIcon} alt="" />
-                                <span style={{ width: 220 }} className="statisticsText">Balance</span>
-                                <span style={{ width: 100, marginLeft: 10, fontWeight: 400, textAlign: "right", color: "#4CC2FF" }} className="statisticsText">64.28 R$</span>
-                            </div>
-
-                        </div>
-
-
-                    </div>
-
+                    <StatusBar marginTop={13} />
                 </div>
             </div>
 
